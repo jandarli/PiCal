26414532bd39f5610d6d09b1318a30a5ab1c872f
jandarli <janicedarling1@gmail.com>
2026-05-09 20:51:28 -0400

    feat: Define wave phase function

--- customcalendar.h
+++ customcalendar.h
@@ -8,9 +8,13 @@ class CustomCalendar : public QCalendarWidget
     Q_OBJECT
     public:
         explicit CustomCalendar(QWidget *parent = nullptr);
+        void setWavePhase(double phase);
 
     protected:
         void paintCell(QPainter *painter, const QRect &rect, const QDate &date) const override;
+
+    private:
+        double m_wavePhase = 0.0;
 };
 
 #endif
